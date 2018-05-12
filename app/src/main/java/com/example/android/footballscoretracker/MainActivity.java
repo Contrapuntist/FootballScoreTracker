ed966f4a428aeba20622b12922d382f385b34bc0
contrapuntist <macano661@gmail.com>
2018-05-12 11:15:24 -0500

    Added horizontal layout and java functionality

--- app/src/main/java/com/example/android/footballscoretracker/MainActivity.java
+++ app/src/main/java/com/example/android/footballscoretracker/MainActivity.java
@@ -7,6 +7,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Global declaration to track points for each team
+     */
     int scoreTeamA = 0;
     int scoreTeamB = 0;
 
@@ -16,36 +19,91 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Method for displaying updates to Team A score
+     * @param score
+     */
     public void teamAScoreTotalDisplay (int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_points_txt);
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * Method for displaying updates to Team B score
+     * @param score
+     */
     public void teamBScoreTotalDisplay (int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_points_txt);
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * Team A method to add touchdown and update TextView
+     */
     public void addTouchdownTeamA (View view) {
         scoreTeamA += 6;
         teamAScoreTotalDisplay(scoreTeamA);
     }
 
+    /**
+     * Team B method to add touchdown and update TextView
+     */
     public void addTouchdownTeamB (View view) {
         scoreTeamB += 6;
         teamBScoreTotalDisplay(scoreTeamB);
     }
 
+    /**
+     * Team A method to add field goal and update TextView
+     */
     public void addFieldGoalTeamA (View view) {
         scoreTeamA += 3;
         teamAScoreTotalDisplay(scoreTeamA);
     }
 
+    /**
+     * Team B method to add field goal and update TextView
+     */
     public void addFieldGoalTeamB (View view) {
         scoreTeamB += 3;
         teamBScoreTotalDisplay(scoreTeamB);
     }
 
+    /**
+     * Team A method to add two points (extra points or touchback) and update TextView
+     */
+    public void addTwoPointsTeamA (View view) {
+        scoreTeamA += 2;
+        teamAScoreTotalDisplay(scoreTeamA);
+    }
+
+    /**
+     * Team B Method to add two points (extra points or touchback) and update TextView
+     */
+    public void addTwoPointsTeamB (View view) {
+        scoreTeamB += 2;
+        teamBScoreTotalDisplay(scoreTeamB);
+    }
+
+    /**
+     * Team A method to add one point and update TextView
+     */
+    public void addOnePointTeamA (View view) {
+        scoreTeamA += 1;
+        teamAScoreTotalDisplay(scoreTeamA);
+    }
+
+    /**
+     * Team B method to add one point and update TextView
+     */
+    public void addOnePointTeamB (View view) {
+        scoreTeamB += 1;
+        teamBScoreTotalDisplay(scoreTeamB);
+    }
+
+    /**
+     * Method resets scores to 0 and updates TextViews for both team a and team b
+     */
     public void trackerReset (View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
@@ -54,4 +112,3 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-
